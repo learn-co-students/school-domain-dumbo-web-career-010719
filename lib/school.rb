2b280b6a2092115ec2e81ba0bc99499afe88c7b3
Norbert Lam <norbertl@buffalo.edu>
2019-01-09 18:28:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  
+   attr_reader :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    if roster.key?(grade) == false
+      roster[grade] = []
+    end
+    roster[grade].push(student_name)
+  end
+  
+  def grade(num)
+    return @roster[num]
+  end
+  
+  def sort
+    @roster.each do |grade, arr|
+      roster[grade] = arr.sort
+    end
+    @roster
+  end
+
+end
