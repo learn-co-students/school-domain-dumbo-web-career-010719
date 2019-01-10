0cac0966ac3b6756955c1f81dd00ed81794a4a2c
Simon Kong <simon.kong95@gmail.com>
2019-01-09 21:19:29 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+
+class School
+    def initialize(name)
+        @name = name
+        @hash = {}
+    end
+
+    def roster
+        @hash
+    end
+
+    def add_student(name, grade)
+        if @hash.has_key?(grade) == false
+            @hash[grade] = []
+            @hash[grade] << name
+        else
+            @hash[grade] << name
+        end
+    end
+
+    def grade(int)
+        @hash[int]
+    end
+
+    def sort
+        @hash.each do |x,y|
+            @hash[x] = y.sort
+        end
+        @hash.sort.to_h
+        
+    end
+    
+end
+
+school = School.new("Bayside High School")
