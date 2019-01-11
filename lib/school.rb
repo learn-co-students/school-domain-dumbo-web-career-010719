6961f9d394ba7c52039baedaa472fd50a9842284
github username <github email address>
2019-01-10 22:54:53 -0500

    Tests passed.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+  # attr_reader :@name
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+    # binding.pry
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sort_hash = {}
+    roster.each do |grade, student|
+      sort_hash[grade] = student.sort
+    end
+    sort_hash
+  end
+end
