637c1464f4cd436da3f69182afcb251086ae181c
Chris Kakos <ckakos@gmail.com>
2019-01-11 17:29:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+# School = Domain Model context
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+
+    def add_student(student_name, grade)
+      roster[grade] ||= []
+      roster[grade] << student_name
+    end
+
+    # ||= 'or equals'
+    def grade(student_grade)
+      roster[student_grade]
+    end
+
+    def sort
+      sorted = {}
+      roster.each do |g, s|
+        sorted[g] = s.sort
+      end
+      sorted
+    end
+end
+
+school = School.new("Bayside High School")
